e664a05a1ba712cde32fd36b4729101731710a65
devzeus <hieu01bdvn@gmail.com>
2025-07-08 15:43:53 +0700

    first commit

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# Biến = Variables
+# Biến là một tên đại diện cho một giá trị nào đó
+hello = "Hello, World!"
+number = 42 # Số nguyên
+pi = 3.14 # Số thực
+is_student = True # Biến kiểu boolean
+
+fruits = ["apple", "banana", "cherry"] # Danh sách = Array
+for fruit in fruits:
+    print(fruit)
